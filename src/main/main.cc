cc09b694eea8f29c9cdd7066c6bfcd7d33368248
orzzamir <30836468+orzzamir@users.noreply.github.com>
2020-04-12 18:34:16 +0300

    Update main.cc

--- src/main/main.cc
+++ src/main/main.cc
@@ -80,7 +80,7 @@ int main(int argc, char** argv)
 {
     const std::string intro_help(
         std::string("\nGNSS-SDR is an Open Source GNSS Software Defined Receiver\n") +
-        "Copyright (C) 2010-2019 (see AUTHORS file for a list of contributors - orz)\n" +
+        "Copyright (C) 2010-2019 (see AUTHORS file for a list of contributors - my5)\n" +
         "This program comes with ABSOLUTELY NO WARRANTY;\n" +
         "See COPYING file to see a copy of the General Public License\n \n");
 
